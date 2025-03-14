ea4f575d33febfb17e2d7dcf4785481ab19b9ba1
kad-0312 <khoaanhdo0312@gmail.com>
2025-03-14 14:59:16 +0700

    Default login

---
+++ app/login/page.tsx
@@ -0,0 +1,102 @@
+"use client";
+import React, {useState} from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Eye, EyeOff } from "lucide-react";
+import { PasswordInput } from "@/components/ui/PasswordInput";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters." })
+    .refine((value) => /[A-Z]/.test(value), {
+      message: "Password must contain at least one uppercase letter.",
+    })
+    .refine((value) => /[0-9]/.test(value), {
+      message: "Password must contain at least one number.",
+    })
+    .refine((value) => /[^A-Za-z0-9]/.test(value), {
+      message: "Password must contain at least one special character.",
+    }),
+});
+
+const page = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const togglePasswordVisibility = () => { setShowPassword(!showPassword); };
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <PasswordInput
+                    placeholder="Password"
+                    {...field}
+                  />                  
+                </FormControl>
+                <FormDescription>
+                  Password must be at least 8 characters, contain at least one
+                  uppercase letter, one number, and one special character.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default page;
