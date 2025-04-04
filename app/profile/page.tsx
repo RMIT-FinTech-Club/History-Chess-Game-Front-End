f914af73e831f86c9e1fae03f1dc19101d46fe6b
kad-0312 <khoaanhdo0312@gmail.com>
2025-04-04 22:47:46 +0700

    Add new UI components and styles for icons and player profiles
    - Implement CupIcon, GamePadIcon, and SettingIcon components with customizable fill and dimensions.
    - Create OtherPlayers component to display a list of online players with dynamic styling based on state.
    - Introduce Tooltip component using Radix UI for enhanced user interaction.
    - Add animation styles in animation.css for various UI effects.
    - Define profile-related styles in profile.module.css, including scrollbar customization and profile icon backgrounds.
    - Add SVG assets for profile icons 1 to 4.

---
+++ app/profile/page.tsx
@@ -0,0 +1,263 @@
+"use client"
+
+import styles from "@/css/profile.module.css"
+import { useRef, useState } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons"
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
+import CountUp from "react-countup";
+import GamePadIcon from "@/components/ui/gamePadIcon"
+import CupIcon from "@/components/ui/cupIcon"
+import SettingIcon from "@/components/ui/settingIcon"
+import OtherPlayers from "@/components/ui/otherPlayers"
+
+const matches = [
+    {
+        opponent: 'Negic Legend',
+        avt: 'https://i.imgur.com/RoRONDn.jpeg',
+        mode: 'Rapid',
+        time: 683,
+        victory: true
+    },
+    {
+        opponent: 'Negic LegendLegendLegendLegendLegendLegendLegend',
+        avt: 'https://i.imgur.com/RoRONDn.jpeg',
+        mode: 'Rapid',
+        time: 683,
+        victory: false
+    },
+    {
+        opponent: 'Negic Legend',
+        avt: 'https://i.imgur.com/RoRONDn.jpeg',
+        mode: 'Rapid',
+        time: 683,
+        victory: true
+    },
+    {
+        opponent: 'Negic Legend',
+        avt: 'https://i.imgur.com/RoRONDn.jpeg',
+        mode: 'Rapid',
+        time: 683,
+        victory: true
+    },
+    {
+        opponent: 'Negic Legend',
+        avt: 'https://i.imgur.com/RoRONDn.jpeg',
+        mode: 'Rapid',
+        time: 683,
+        victory: true
+    },
+    {
+        opponent: 'Negic Legend',
+        avt: 'https://i.imgur.com/RoRONDn.jpeg',
+        mode: 'Rapid',
+        time: 683,
+        victory: true
+    },
+    {
+        opponent: 'Negic Legend',
+        avt: 'https://i.imgur.com/RoRONDn.jpeg',
+        mode: 'Rapid',
+        time: 683,
+        victory: true
+    },
+    {
+        opponent: 'Negic Legend',
+        avt: 'https://i.imgur.com/RoRONDn.jpeg',
+        mode: 'Rapid',
+        time: 683,
+        victory: true
+    },
+    {
+        opponent: 'Negic Legend',
+        avt: 'https://i.imgur.com/RoRONDn.jpeg',
+        mode: 'Rapid',
+        time: 683,
+        victory: true
+    },
+    {
+        opponent: 'Negic Legend',
+        avt: 'https://i.imgur.com/RoRONDn.jpeg',
+        mode: 'Rapid',
+        time: 683,
+        victory: true
+    },
+    {
+        opponent: 'Negic Legend',
+        avt: 'https://i.imgur.com/RoRONDn.jpeg',
+        mode: 'Rapid',
+        time: 683,
+        victory: true
+    },
+]
+
+export default function ProfilePage() {
+    const profileRef = useRef<HTMLDivElement | null>(null)
+    const [isProfileOpened, setIsProfileOpened] = useState<boolean>(true)
+    const [profileMenu, setProfileMenu] = useState(1)
+
+    const handleToggleProfile = () => setIsProfileOpened(!isProfileOpened)
+
+    return (
+        <div className="w-[90vw] md:w-[80vw] overflow-hidden flex flex-col py-[3vh] mx-[5vw] md:mx-[10vw] text-white relative h-[100dvh]">
+            <div className={`w-full relative md:absolute ${isProfileOpened ? 'md:top-[3vh]' : 'md:top-[calc(-12vw-2px)]'} top-0 left-0 flex items-center rounded-[2vw] h-[15vw] md:h-[12vw] bg-[#1D1D1D] border border-solid border-[#77878B] mb-[3vh] transition-all duration-300`}>
+                <Tooltip disableHoverableContent>
+                    <TooltipTrigger asChild>
+                        <div
+                            ref={profileRef}
+                            onClick={handleToggleProfile}
+                            className={`absolute w-[calc(2vw-2px)] aspect-square left-[calc(100%-2vw)] bottom-[calc(-3vw)] cursor-pointer bg-[#1D1D1D] border border-solid border-white rounded-[50%] hidden md:flex justify-center items-center`}
+                        >
+                            <FontAwesomeIcon
+                                icon={faArrowUp}
+                                className={`${isProfileOpened ? 'rotate-none' : 'rotate-[180deg]'} text-[1vw]`}
+                            />
+                        </div>
+                    </TooltipTrigger>
+                    <TooltipContent
+                        side="bottom"
+                        className="text-white bg-black rounded-lg px-2 py-1 shadow-lg"
+                    >
+                        {isProfileOpened ? "Close Profile" : "Open Profile"}
+                    </TooltipContent>
+                </Tooltip>
+                <div className="w-[35vw] md:w-[32vw] flex justify-between items-center">
+                    <div
+                        style={{ backgroundImage: `url(https://i.imgur.com/RoRONDn.jpeg)` }}
+                        className="w-[10vw] md:w-[8vw] m-[2vw] aspect-square rounded-[50%] bg-center bg-cover bg-no-repeat border border-white border-solid"
+                    ></div>
+                    <div className="w-[23vw] md:w-[24vw] flex flex-col justify-between items-start">
+                        <p className="text-[2vw] font-bold w-full whitespace-nowrap overflow-hidden text-ellipsis">Negic Legend</p>
+                        <p className="text-[1.2vw] font-thin my-[0.5vw] md:my-0">Global Ranking: #100</p>
+                        <p className="text-[1.2vw] font-thin">Player ID: 31082007</p>
+                    </div>
+                </div>
+                <div className="w-[65vw] md:w-[48vw] flex justify-around items-center py-[2vw]">
+                    {[
+                        {
+                            icon: styles.profile_icon_1,
+                            content: 'Level',
+                            number: 5,
+                        },
+                        {
+                            icon: styles.profile_icon_2,
+                            content: 'Game Mode',
+                            number: '1vs1'
+                        },
+                        {
+                            icon: styles.profile_icon_3,
+                            content: 'Wallet',
+                            number: 500,
+                            prefix: 'K'
+                        },
+                        {
+                            icon: styles.profile_icon_4,
+                            content: 'Won Matches',
+                            number: 120
+                        },
+                    ].map((card, index) => (
+                        <div key={index} className="w-[10vw] md:w-[8vw] h-[11vw] md:h-[10vw] flex flex-col justify-center items-center bg-black rounded-[1vw] border border-solid border-[#77878B]">
+                            <div className={`w-[2.5vw] aspect-square bg-center bg-contain bg-no-repeat ${card.icon}`}></div>
+                            <p className="text-[1.2vw] md:text-[1vw] text-[#77878B] my-[0.5vh] md:my-[1vh]">{card.content}</p>
+                            <p className="text-[1.5vw] leading-[1vw]">
+                                {typeof(card.number) === 'number' ? <CountUp start={0} end={card.number} useEasing={false} duration={2} /> : `${card.number}`}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className={`w-full ${isProfileOpened ? 'md:mt-[calc(12vw+2px+3vh)]' : 'md:mt-0'} mt-0 ${isProfileOpened ? 'h-[calc(100vh-3vh-12vw-2px-6vh)]' : 'h-[calc(100vh-6vh)]'} transition-all duration-300 flex flex-col md:flex-row justify-start md:justify-between`}>
+                <div className="md:w-[30%] w-full flex flex-col">
+                    <div className="px-0 md:px-[2vw] py-[1vw] md:py-[2vw] w-full flex flex-row md:flex-col bg-[#1D1D1D] rounded-[2vw] mb-[3vh] relative items-center justify-around">
+                        <div className={`absolute ${profileMenu === 0 ? 'md:top-[2vw] md:left-0 top-0 left-0' : `${profileMenu === 1 ? 'md:top-[6vw] md:left-0 top-0 left-[30vw]' : 'md:top-[10vw] md:left-0 top-0 left-[60vw]'}`} left-0 md:h-[2vw] md:w-[calc(2vw/3)] w-1/3 h-[1vw] rounded-[0.5vw] md:rounded-[1vw] bg-[#DBB968] transition-all duration-200`}></div>
+                        {[
+                            {
+                                icon: CupIcon,
+                                content: 'Statistic'
+                            },
+                            {
+                                icon: GamePadIcon,
+                                content: 'Matches'
+                            },
+                            {
+                                icon: SettingIcon,
+                                content: 'Account Settings'
+                            },
+                        ].map((menu, index) => (
+                            <div
+                                key={index}
+                                className={`flex justify-center mr-0 md:mr-auto w-1/3 md:w-[max-content] max-w-[100%] cursor-pointer group ${index % 2 === 1 ? 'my-[2vw]' : ''}`}
+                                onClick={() => { if(index !== profileMenu) setProfileMenu(index) }}
+                            >
+                                <div className={`${index === profileMenu ? `${styles.profile_menu_icon} border-[#DBB968]` : 'border-white'} h-[calc(4vw-2px)] md:h-[calc(2vw-2px)] aspect-square flex justify-center items-center border border-solid rounded-[50%]`}>
+                                    {<menu.icon width="60%" fill={`${index === profileMenu ? '#DBB968' : 'white'}`} />}
+                                </div>
+                                <p className={`${index === profileMenu ? `text-[#DBB968]` : 'text-white group-hover:md:left-[0.3vw]'} text-[2.5vw] leading-[4vw] md:text-[2vw] md:leading-[2vw] ml-[1vw] max-w-[100%] whitespace-nowrap overflow-hidden text-ellipsis relative left-0 group-hover:text-[#DBB968] transition-all duration-200`}>{menu.content}</p>
+                            </div>
+                        ))}
+                    </div>
+                    
+                    {profileMenu !== 1 && (<OtherPlayers isOpen={isProfileOpened} />)}
+                
+
+                    <div className="p-[2vw] w-full h-[100%] hidden md:flex flex-col bg-[#1D1D1D] rounded-[2vw]">
+                        <p className="text-[2vw] leading-[2vw] mb-[3vh]">Other Players</p>
+                        <div className={`flex flex-col w-full ${isProfileOpened ? 'h-[calc(100vh-12vw-3vh-2px-3vh-14vw-6vh-6vw-3vh)]' : 'h-[calc(100vh-3vh-3vh-14vw-6vh-6vw-3vh)]'} overflow-y-auto overflow-x-hidden transition-all duration-300 ${styles.list_container}`}>
+                            {Array.from({ length: 20 }).map((_, index) => (
+                                <div key={index} className={`flex relative ${styles.online_player} justify-start items-center ${index == 19 ? 'mb-0' : 'mb-[3vh]'}`}>
+                                    <div
+                                        style={{ backgroundImage: `url(https://i.imgur.com/RoRONDn.jpeg)` }}
+                                        className="w-[calc(3vw-2px)] bg-center bg-cover bg-no-repeat aspect-square rounded-[50%] border border-solid border-white"
+                                    ></div>
+                                    <p className="w-full mx-[1vw] text-[1.2vw] whitespace-nowrap overflow-hidden text-ellipsis">{index == 19 ? 'Negic Legend Legend Legend' : 'Negic Legend'}</p>
+                                    <p className="text-[1.2vw] text-[#C4C4C4] md:hover:text-[#DBB968] hover:text-white cursor-pointer">Challenge</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className="w-full md:w-[60%] flex flex-col">
+                    <div className="flex items-center">
+                        <GamePadIcon width="3vw" />
+                        <p className="text-[3vw] leading-[3vw] ml-[1vw]">Matches</p>
+                    </div>
+                    <div className={`flex flex-col w-full h-[calc(100dvh-3vh-15vw-3vh-6vw-3vh-3vw-2vh+4px-6vh)] md:h-[100%] overflow-y-auto mt-[2vh] ${styles.list_container}`}>
+                        {matches.map((match, index) => (
+                            <div
+                                key={index}
+                                className={`${index !== matches.length - 1 ? 'mb-[3vh]' : 'mb-0'} ${styles.match} w-full rounded-[1vw] bg-[rgba(0,0,0,0.5)] border border-solid ${match.victory ? `border-[#1CFF07] ${styles.victory}` : 'border-[#EA4335]'}`}
+                            >
+                                <div className="w-full flex items-center justify-start px-[2vw] md:px-[1vw] rounded-[1vw] overflow-y-hidden">
+                                    <div
+                                        style={{ backgroundImage: `url(${match.avt})` }}
+                                        className="w-[calc(8vw-2px)] md:w-[calc(4vw-2px)] my-[2vw] md:my-[1vw] aspect-square rounded-[50%] bg-center bg-cover bg-no-repeat border border-white border-solid mr-[2vw] md:mr-[1vw]"
+                                    ></div>
+                                    <div className="w-[100%] flex justify-between items-center mr-[3vw]">
+                                        <div className="flex flex-col justify-center items-start mr-[2vw]">
+                                            <p className="text-[1.8vw] md:text-[1vw] text-[#C4C4C4]">Opponent</p>
+                                            <p className="text-[1.8vw] md:text-[1vw] font-bold w-[30vw] md:w-[15vw] whitespace-nowrap overflow-hidden text-ellipsis">{match.opponent}</p>
+                                        </div>
+                                        <div className="flex justify-between items-center w-[100%]">
+                                            <div className="flex flex-col justify-center items-start">
+                                                <p className="text-[1.8vw] md:text-[1vw] text-[#C4C4C4]">Game Mode</p>
+                                                <p className="text-[1.8vw] md:text-[1vw] font-bold">{match.mode}</p>
+                                            </div>
+                                            <div className="flex flex-col justify-center items-start">
+                                                <p className="text-[1.8vw] md:text-[1vw] text-[#C4C4C4]">Time</p>
+                                                <p className="text-[1.8vw] md:text-[1vw] font-bold">{`${Math.floor(match.time / 60)}:${match.time % 60}`}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="w-[22vw] md:w-[16vw] flex justify-center items-center">
+                                        <p className={`text-[1.5vw] px-[2vw] mr-[1vw] relative ${match.victory ? 'text-[#1CFF07]' : 'text-[#EA4335]'} font-bold ${styles.result}`}>{match.victory ? 'Victory' : 'Defeat'}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
