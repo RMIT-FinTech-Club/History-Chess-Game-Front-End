995f6b1fa9e13fd7313960461bc7f9385e7e3c33
Aro9 <89306536+nhatxminh@users.noreply.github.com>
2025-04-10 18:04:35 +0700

    Update background & Yellow Light

--- app/game/offline/page.tsx
+++ app/game/offline/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ChessboardComponent from '../../../components/ChessboardComponent';
+import YellowLight from '../../../components/ui/YellowLight';
 
 export default function GamePage() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <YellowLight top='25%' left='65%' />
       <ChessboardComponent />
     </div>
   );
