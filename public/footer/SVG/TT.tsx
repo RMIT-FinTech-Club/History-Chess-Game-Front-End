4d8148f58172099cb7bf0e52bfec93ac419635d4
Negic Legend <negiclegend2@gmail.com>
2025-04-23 03:33:32 +0700

    update footer

---
+++ public/footer/SVG/TT.tsx
@@ -0,0 +1,14 @@
+export default function YT_icon() {
+    return (
+        <svg className="md:w-[2vw] md:h-auto h-[8vw] w-auto transition-colors duration-200 hover:stroke-[#DBB968] stroke-white aspect-square" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_14_8473)">
+                <path d="M30.7015 4.72338C29.4236 5.62478 28.0087 6.31421 26.5113 6.76511C25.7076 5.84101 24.6394 5.18603 23.4514 4.88875C22.2633 4.59148 21.0126 4.66626 19.8684 5.10297C18.7242 5.53969 17.7417 6.31727 17.0539 7.33054C16.366 8.34382 16.006 9.54391 16.0224 10.7685V12.103C13.6772 12.1638 11.3534 11.6437 9.25798 10.5889C7.16252 9.53423 5.36042 7.97765 4.01219 6.05784C4.01219 6.05784 -1.32567 18.068 10.6845 23.4059C7.93622 25.2714 4.66228 26.2068 1.34326 26.0748C13.3534 32.7471 28.0325 26.0748 28.0325 10.7285C28.0313 10.3568 27.9956 9.98597 27.9258 9.62086C29.2877 8.27772 30.2489 6.58191 30.7015 4.72338V4.72338Z" stroke-width="1.33446" stroke-linecap="round" stroke-linejoin="round" />
+            </g>
+            <defs>
+                <clipPath id="clip0_14_8473">
+                    <rect width="32.0271" height="32.0271" fill="white" transform="translate(0.00878906 0.720215)" />
+                </clipPath>
+            </defs>
+        </svg>
+    )
+}
